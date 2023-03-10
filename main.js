02d12951708c86eacbf82e6974c469e9479c212a
JuanJoseDuranRinconCAMPUS2 <jduranrinconcampus@gmail.com>
2023-03-10 06:43:25 -0500

    feat: :art: Taller-2 Ejercicio-2
    Taller-2 Ejercicio-2

--- main.js
+++ main.js
@@ -1,22 +1,30 @@
 
-console.log("%c¡Ejercicio 1 !",
+console.log("%c¡Ejercicio 2 !",
 "background:linear-gradient(#000, #FF8000); color:#fff; padding: 5px 10px;");
-console.log("Construir el algoritmo para un programa que ingrese tres notas de un alumno, si el promedio es menor o igual a 3.9 mostrar un mensaje `Estudie`, de lo contrario un mensaje que diga `becado`");
-const Alpha = 0;
-function promedio(){
-    console.log("Bienvenido estudiante por favor sigue las siguientes instrucciones")
-    const Calificaciones = []
-    let Notas = prompt("Ingresa el numero de notas")
-    for (let i = 1; i <= Notas; i++) {
-        let Nota = parseInt(prompt(`Ingresa la nota ${i} del estudiante  `));
-         Calificaciones.push(Nota);
-     }
+console.log("Dado un número indicar si es par o impar y si es mayor de 10.");
 
-    SumaTotal = Calificaciones.reduce((accumulator, currentValue) => accumulator + currentValue, Alpha);
-    Promedio = SumaTotal / Notas;
-    console.log(`El promedio del Estudiante es el siguiente`)
-    console.log(Promedio);
 
-}
+let num = parseInt(prompt("Ingrese el Numero  "));
+let DV = num%2;
 
-promedio()
+function par(){
+    console.log("Numero ingresado")
+    console.log(DV)
+    console.log("Los resultados son los siguientes")
+    switch (DV){
+        case 0:
+            console.log("El Numero Es Par :3")
+        break;
+        default:
+            console.log("El Numero Es Impar :d")
+        break;
+    };
+    if (num > 10){
+        console.log("El Numero Es Mayor Que 10")
+    } else{
+        console.log("El Numero Es Menor Que 10")
+    }
+    return
+};
+
+par();
