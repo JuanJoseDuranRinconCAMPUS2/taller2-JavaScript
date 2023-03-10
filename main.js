d28b4641ab6e1bd3bfd55bf71d5fadbda017d2a3
JuanJoseDuranRinconCAMPUS2 <jduranrinconcampus@gmail.com>
2023-03-10 07:34:56 -0500

    feat: :art: Taller-2 Ejercicio-4
    Taller-2 Ejercicio-4

--- main.js
+++ main.js
@@ -1,21 +1,37 @@
 
-console.log("%c¡Ejercicio 3 !",
+console.log("%c¡Ejercicio 4 !",
 "background:linear-gradient(#000, #FF8000); color:#fff; padding: 5px 10px;");
-console.log("Construir el algoritmo para determinar el voltaje de un circuito a partir de la resistencia y la intensidad de corriente.");
+console.log("Construir el algoritmo que solicite el nombre y edad de 3 personas y determine el nombre de la persona con mayor edad.");
 
 
-let I = parseInt(prompt("Ingrese el Valor de la Intencidad de la Corriente  "));
-let R = parseInt(prompt("Ingrese el Valor de la Resistencia  "));
+
+function Edad(){
+    
+    let Nom1 = prompt(`Ingresa el nombre de la persona 1  `);
+    let Edad1 = parseInt(prompt(`Ingresa la Edad de la persona 1   `));
+    console.log(`Primera persona registrada "${Nom1}" con una edad de ${Edad1}`);
+    let Nom2 = prompt(`Ingresa el nombre de la persona 2  `);
+    let Edad2 = parseInt(prompt(`Ingresa la Edad de la persona 2   `));
+    console.log(`Segunda persona registrada "${Nom2}" con una edad de ${Edad2}`);
+    let Nom3 = prompt(`Ingresa el nombre de la persona 3  `);
+    let Edad3 = parseInt(prompt(`Ingresa la Edad de la persona 3   `));
+    console.log(`Tercera persona registrada "${Nom3}" con una edad de ${Edad3}`);
 
 
-function Voltaje(){
-    console.log("Intencidad Ingresada")
-    console.log(I)
-    console.log("Resistencia Ingresada")
-    console.log(R)
-    console.log("-----------------Calculando----------------")
-    let V = I * R
-    console.log(`Segun la intencidad registrada de  ${I} y la resistencia registrada de ${R} se determina el valor del Voltaje de ${V}`)
+    edades = [Edad1, Edad2, Edad3]
+    max = Math.max(...edades)
+    switch(max){
+        case Edad1 :
+            console.log(`La persona ${Nom1} es la mayor del grupo con una edad de ${Edad1}`)
+        break;
+        case Edad2:
+            console.log(`La persona ${Nom2} es la mayor del grupo con una edad de ${Edad2}`)
+        break;
+        case Edad3 :
+            console.log(`La persona ${Nom3} es la mayor del grupo con una edad de ${Edad3}`)
+        break;
+    }
+       
 };
 
-Voltaje();
+Edad();
