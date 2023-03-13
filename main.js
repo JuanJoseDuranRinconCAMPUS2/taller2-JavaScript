36d95ebca1c99b51d760cbea9fc61e1fbcab2160
JuanJoseDuranRinconCAMPUS2 <jduranrinconcampus@gmail.com>
2023-03-13 06:24:45 -0500

    feat: :art: Taller-2 Ejercicio-7
    Taller-2 Ejercicio-7

--- main.js
+++ main.js
@@ -1,48 +1,24 @@
 
-console.log("%c¡Ejercicio 6 !",
+console.log("%c¡Ejercicio 7 !",
 "background:linear-gradient(#000, #FF8000); color:#fff; padding: 5px 10px;");
-console.log("Construir el algoritmo en Javascript para un programa para cualquier cantidad de estudiantes que lea el nombre, el sexo y la nota definitiva y halle al estudiante con la mayor nota y al estudiante con la menor nota y cuantos eran hombres y cuantos mujeres.");
+console.log("Programa que pida el ingreso del nombre y precio de un artículo y la cantidad que lleva el cliente. Mostrar lo que debe pagar el comprador en su factura.");
 
-let Nombres = [];
-let Sexo = [];
-let Notas = [];
-var Alpha = 0;
-function definitiva(){
-    do{
-        console.log("Hola Por favor ingresa los datos de tus estudiantes")
-        let Nom= prompt("Ingrese el nombre del estudiante    ");
-        let Genero = prompt("Ingrese el Sexo del estudiante (M o F)    ");
-        let Nota = parseFloat(prompt("Ingresa la Calificacion final del estudiante  "));
-        Nombres.push(Nom)
-        Sexo.push(Genero)
-        Notas.push(Nota)   
-        BD = Nombres.concat(Sexo, Notas)
-        for(i = 0; i < Notas.length; i++){
-
-            if (Notas[i] > Alpha)
-        
-            {
-        
-                Alpha = Notas[i];
-        
-            }
-        
-        }
-
-    }while(confirm("¿Desea ingresar otro estudiante?"))
-    console.log("A continuacion la informacion registrada")
-    console.log(BD)
-    let IndexMayor = Notas.indexOf(Alpha); 
-    console.log("--------------------CALCULANDO----------------------------");
-    console.log("Resultados");
-    if (Sexo[IndexMayor] == "M" || Sexo[IndexMayor] == "m"){
-        console.log(`El Estudiante ${Nombres[IndexMayor]} de genero registrado de ${Sexo[IndexMayor]} es el estudiante con mayor nota con una calificacion final de ${Notas[IndexMayor]}`);
-    }else{
-        console.log(`la Estudiante ${Nombres[IndexMayor]} de genero registrado de ${Sexo[IndexMayor]} es la estudiante con mayor nota con una calificacion final de ${Notas[IndexMayor]}`);
+function facturacion(){
+    let Nombre = prompt("Ingresa Tu nombre:   ");
+    let ID = prompt("Ingresa Tu identificación:   ");
+    let NA = prompt("Ingresa el Nombre del Artículo:   ");
+    let PA = parseInt(prompt("Ingresa el Precio del Articulo: COP$"));
+    let CA= parseInt(prompt("Ingresa la Cantidad de artículos comprados:   "));
+    function random(min, max) {
+        return Math.floor((Math.random() * (max - min + 1)) + min);
     }
-        
+    Total = CA * PA;
+    console.log("--------------GENERANDOFACTURA--------------");
+    console.log(`\t TIENDA ORIGINAL  NIT: 2344234234 \n \t Tienda Sur TEL: ########## \n \t Comprador: ${Nombre} \n \t CC: ${ID} \n \tCODIGO----DESCRIPCION----UNIDADES----VALOR`);
+    console.log(`\t ${random(1000,10000)}      ${NA}      ${CA}      ${PA}`);
+    console.log("\t--------------TOTAL--------------");
+    console.log(`\t Total a pagar      COP$${Total}`)
+
 }
 
-definitiva()
-
-
+facturacion()
