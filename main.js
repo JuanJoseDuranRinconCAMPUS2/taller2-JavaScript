7674db00ce5d3b45a2d243ccbf13c5c45532e913
JuanJoseDuranRinconCAMPUS2 <jduranrinconcampus@gmail.com>
2023-03-13 06:28:21 -0500

    feat: :art: Taller-2 Ejercicio-8
    Taller-2 Ejercicio-8

--- main.js
+++ main.js
@@ -1,24 +1,25 @@
 
-console.log("%c¡Ejercicio 7 !",
+console.log("%c¡Ejercicio 8 !",
 "background:linear-gradient(#000, #FF8000); color:#fff; padding: 5px 10px;");
-console.log("Programa que pida el ingreso del nombre y precio de un artículo y la cantidad que lleva el cliente. Mostrar lo que debe pagar el comprador en su factura.");
+console.log("Programa que Ingrese por teclado: a. el valor del lado de un cuadrado para mostrar por pantalla el perímetro del mismo b. la base y la altura de un rectángulo para mostrar el área del mismo");
 
-function facturacion(){
-    let Nombre = prompt("Ingresa Tu nombre:   ");
-    let ID = prompt("Ingresa Tu identificación:   ");
-    let NA = prompt("Ingresa el Nombre del Artículo:   ");
-    let PA = parseInt(prompt("Ingresa el Precio del Articulo: COP$"));
-    let CA= parseInt(prompt("Ingresa la Cantidad de artículos comprados:   "));
-    function random(min, max) {
-        return Math.floor((Math.random() * (max - min + 1)) + min);
+function FacilisimoVerdad(){
+    console.log("--------------Calculadora de Perimetro y Area--------------");
+    let Operacion = prompt(`Si quieres calcular el Perimetro de un cuadrado digita "P" y Si quieres calcular el Area de un triangulo digita "A"`)
+    if (Operacion == "P" || Operacion=="p"){
+        console.log("--------------Sistema de calculo del Perimetro de un cuadrado--------------");
+        let L = parseFloat(prompt("Ingresa el Valor del lado del cuadrado"));
+        console.log(`-----------Realizando Operacion P=${L}*4cm--------------`);
+        console.log(`Resultado: El perimetro es de ${L*4}cm`);
+    }else if (Operacion == "A" || Operacion=="a") {
+        console.log("--------------Sistema de calculo del Area de un Triangulo--------------");
+        let B = parseFloat(prompt("Ingresa el Valor de la Base del Triangulo"));
+        let H = parseFloat(prompt("Ingresa el Valor de la Altura del Triangulo"));
+        console.log(`-----------Realizando Operacion A= (${B}*${H})/2cm--------------`);
+        console.log(`Resultado: El Area es de ${(B*H)/2}cm3`);
+    }else {
+        console.log("ERROR datos no validos 404")
     }
-    Total = CA * PA;
-    console.log("--------------GENERANDOFACTURA--------------");
-    console.log(`\t TIENDA ORIGINAL  NIT: 2344234234 \n \t Tienda Sur TEL: ########## \n \t Comprador: ${Nombre} \n \t CC: ${ID} \n \tCODIGO----DESCRIPCION----UNIDADES----VALOR`);
-    console.log(`\t ${random(1000,10000)}      ${NA}      ${CA}      ${PA}`);
-    console.log("\t--------------TOTAL--------------");
-    console.log(`\t Total a pagar      COP$${Total}`)
-
 }
 
-facturacion()
+FacilisimoVerdad()
