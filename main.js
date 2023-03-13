4c4809c3e18b4001310cc057ef1b7d8fe14ab663
JuanJoseDuranRinconCAMPUS2 <jduranrinconcampus@gmail.com>
2023-03-13 06:31:23 -0500

    feat: :art: Taller-2 Ejercicio-10
    Taller-2 Ejercicio-10

--- main.js
+++ main.js
@@ -1,50 +1,26 @@
 
-console.log("%c¡Ejercicio 9 !",
+console.log("%c¡Ejercicio 10 !",
 "background:linear-gradient(#000, #FF8000); color:#fff; padding: 5px 10px;");
-console.log("N atletas han pasado a finales en salto triple en los juegos olímpicos femenio de 2022. Diseñe un programa que pida por teclado los nombres de cada atleta finalista y a su vez, sus marcas del salto en metros. Informar el nombre de la atleta campeona que se quede con la medalla de oro y si rompió récord, reportar el pago que será de 500 millones. El récord esta en 15,50 metros.");
+console.log("Desarrolle un programa cíclico que capture un dato numérico cada vez, y los vaya acumulando. El programa se detiene cuando el usuario digita un cero. El programa debe mostrar: LA SUMATORIA DE LOS VALORES, EL VALOR DEL PROMEDIO, CUÁNTOS VALORES FUERON DIGITADOS, MAYOR VALOR Y MENOR VALOR.");
 
-
-function Bienvenida (){
-    console.log("------------------------JuegosOlimpicosSoftware2022-------------------------");
-    console.log("Hola!, Bienvenido al sistema de registro oficial de los juegos olimpicos");
-    console.log("Soy Miraitowa y sere tu asistente virtual en esta ocasion :3");
-}
-
-let Nombres = [];
-let Marcas = [];
-var Alpha = 0;
-function Atletas(){
-    let i = 0
-    do{
-        console.log("Hola Por favor ingresa los datos de las finalistas")
-        let Nom= prompt(`Ingrese el nombre de la finalista ${i+1}   `);
-        let Mar = parseFloat(prompt(`Ingrese la Marca de la finalista ${i+1}   `));
-        Nombres.push(Nom);
-        Marcas.push(Mar);   
-        BD = Nombres.concat(Marcas)
-        for(i = 0; i < Marcas.length; i++){
-
-            if (Marcas[i] > Alpha)
-        
-            {
-        
-                Alpha = Marcas[i];
-        
-            }
-        
+numeros = [];
+const Alpha = 0
+function ciclo(){
+    while(num != 0){
+        var num = parseInt(prompt("Ingresa un numero")) ;
+        if (num != 0){
+            numeros.push(num);
         }
-
-    }while(confirm("¿Desea ingresar otra Finalista?"))
-    console.log("A continuacion la informacion registrada")
-    console.log(BD)
-    let IndexMayor = Marcas.indexOf(Alpha); 
-    console.log("--------------------CALCULANDO----------------------------");
-    console.log(`La Finalista ganadora del Oro es ${Nombres[IndexMayor]} gracias a su marca de ${Marcas[IndexMayor]}mts. FELICIDADES`);
-    if (Marcas[IndexMayor] > 15.50){
-        console.log("INCREIBLE!, rompiste nuestro record anterior, recuerda reclamar tu pago de 500 millones por llegar tan lejos")
     }
-        
-}
+    console.log("---------------Reconociendo Numeros------------------")
+    console.log(`Numeros Ingresados: ${numeros}`)
+    SumaTotal = numeros.reduce((accumulator, currentValue) => accumulator + currentValue, Alpha);
+    TotalV = numeros.length
+    Promedio = SumaTotal / (TotalV);
+    console.log(`La suma de todos los valores es:  ${SumaTotal}`)
+    console.log(`El promedio de los valores ingresados es: ${Promedio}`)
+    console.log(`El Dato mayor es: ${Math.max(...numeros)}`)
+    console.log(`El Dato menor es: ${Math.min(...numeros)}`)
 
-Bienvenida()
-Atletas()
+}
+ciclo()
